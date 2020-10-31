18f33e94d10c8c5de8ae9f866222da0106177ff1
Wator Vapor <watorvapor@gmail.com>
2020-10-31 12:00:24 +0900

    first

---
+++ motor_ctrl/sensored.bldc/eleg.js
@@ -0,0 +1,212 @@
+const filter = [
+  { vendorId: 0x1A86 }
+];
+const serialOption = { 
+  baudRate: 115200,
+  databits:8,
+  stopbits:1,
+  parity:'none',
+  buffersize:128
+};
+
+let gElegWriter = false;
+const onClickOpenDevice = async (evt) => {
+  try {
+    const device = await navigator.serial.requestPort({filter: filter});
+    console.log('onClickOpenDevice::device=<',device,'>');
+    await device.open(serialOption);
+    console.log('onClickOpenDevice::device=<',device,'>');
+    const reader = device.readable.getReader();
+    console.log('onClickOpenDevice::reader=<',reader,'>');
+
+    gElegWriter = device.writable.getWriter();
+    console.log('onClickOpenDevice::gElegWriter=<',gElegWriter,'>');
+
+    setTimeout(()=> {
+      readSerailData(reader);
+    },0);
+  }
+  catch(e) {
+    console.log('onClickListDevice::e=<',e,'>');
+  }
+}
+
+let readBuffOneLine = '';
+let readLineBufferSlice = [];
+let readLineBuffer = [];
+const elemRead = document.getElementById('serail-output');
+const readSerailData = (reader)=> {
+  const readOne = reader.read();
+  readOne.then((evt)=>{
+    //console.log('readSerailData::readOne evt=<',evt,'>');
+    //console.log('readSerailData::readOne evt.value=<',evt.value.buffer,'>');
+    const utf8txt = new TextDecoder("utf-8").decode(evt.value);
+    //console.log('readSerailData::readOne utf8txt=<',utf8txt,'>');
+    readBuffOneLine += utf8txt;
+    const buffParam = readBuffOneLine.split('\n');
+    //console.log('readSerailData::readOne buffParam=<',buffParam,'>');
+    if(buffParam.length > 1) {
+      for(const lineCmd of buffParam) {
+        //console.log('readSerailData::readOne lineCmd=<',lineCmd,'>');
+        if(lineCmd) {
+          readLineBuffer.push(lineCmd);
+          onELegInfoLine(lineCmd);
+        }
+      }
+      elemRead.textContent = readLineBuffer.reverse().join('\n');
+      const lastElem = buffParam[buffParam.length -1];
+      //console.log('readSerailData::readOne lastElem=<',lastElem,'>');
+      readBuffOneLine = lastElem;
+    }
+    if(evt.done === false) {
+      setTimeout(()=>{
+        readSerailData(reader);
+      },0)
+    }
+  });          
+};
+
+const ElegPosCounterStarter = 'iPositionByHall=<';
+const ElegPosLowStarter = 'iPositionByHallRangeLow=<';
+const ElegPosHighStarter = 'iPositionByHallRangeHigh=<';
+const ElegSpeedStarter = 'iConstCurrentSpeed=<';
+
+const onELegInfoLine = (lineCmd) => {
+  //console.log('onELegInfoLine::lineCmd=<',lineCmd,'>');
+  if( lineCmd.indexOf(ElegPosCounterStarter) >= 0) {
+    const currentPos = getValueOfLineCmd(lineCmd);
+    //console.log('onELegInfoLine::currentPos=<',currentPos,'>');
+    updateRangevalue(currentPos);
+  }
+  if(lineCmd.indexOf(ElegPosLowStarter) >= 0) {
+    const lowPos = getValueOfLineCmd(lineCmd);
+    console.log('onELegInfoLine::lowPos=<',lowPos,'>');
+    changeLowOfPos(lowPos);
+  }
+  if(lineCmd.indexOf(ElegPosHighStarter) >= 0) {
+    const hightPos = getValueOfLineCmd(lineCmd);
+    console.log('onELegInfoLine::hightPos=<',hightPos,'>');
+    changeHighOfPos(hightPos);
+  }
+  if(lineCmd.indexOf(ElegSpeedStarter) >= 0) {
+    const speed = getValueOfLineCmd(lineCmd);
+    console.log('onELegInfoLine::speed=<',speed,'>');
+  }
+}
+
+const getValueOfLineCmd =(lineCmd) => {
+  const start = lineCmd.indexOf('=<');
+  const end = lineCmd.indexOf('>');
+  if(start > -1 && end > -1) {
+    const value = lineCmd.slice(start+2,end);
+    //console.log('onELegInfoLine::value=<',value,'>');
+    return parseInt(value);
+  }
+  return false;
+}
+
+const changeLowOfPos = (value) => {
+  const minElem = document.getElementById('eleg-position-min');
+  minElem.textContent = value;
+  const rangeElem = document.getElementById('eleg-position-value-range');
+  rangeElem.setAttribute('min',parseInt(value) + 1);
+}
+
+
+const changeHighOfPos = (value) => {
+  const maxElem = document.getElementById('eleg-position-max');
+  maxElem.textContent = value;
+  const rangeElem = document.getElementById('eleg-position-value-range');
+  rangeElem.setAttribute('max',parseInt(value) -1);
+}
+
+const onClickCalibratePosition = (evt) => {
+  try {
+  if(gElegWriter) {
+    const reqcalibrate = 'calibrate\n';
+    const wBuff = new TextEncoder().encode(reqcalibrate);
+    console.log('onClickCalibratePosition::wBuff=<',wBuff,'>');
+    gElegWriter.write(wBuff);
+  }
+  }
+  catch(e) {
+    console.log('onClickCalibratePosition::e=<',e,'>');
+  }
+}
+
+const onClickTurnZ = (evt) => {
+  try {
+  if(gElegWriter) {
+    const reqcalibrate = 'z\n';
+    const wBuff = new TextEncoder().encode(reqcalibrate);
+    console.log('onClickTurnZ::wBuff=<',wBuff,'>');
+    gElegWriter.write(wBuff);
+  }
+  }
+  catch(e) {
+    console.log('onClickTurnZ::e=<',e,'>');
+  }
+}
+
+const onClickTurnF = (evt) => {
+  try {
+  if(gElegWriter) {
+    const reqcalibrate = 'f\n';
+    const wBuff = new TextEncoder().encode(reqcalibrate);
+    console.log('onClickTurnF::wBuff=<',wBuff,'>');
+    gElegWriter.write(wBuff);
+  }
+  }
+  catch(e) {
+    console.log('onClickTurnF::e=<',e,'>');
+  }
+}
+
+const onClickTurnG = (evt) => {
+  try {
+  if(gElegWriter) {
+    const reqcalibrate = 'g\n';
+    const wBuff = new TextEncoder().encode(reqcalibrate);
+    console.log('onClickTurnG::wBuff=<',wBuff,'>');
+    gElegWriter.write(wBuff);
+  }
+  }
+  catch(e) {
+    console.log('onClickTurnG::e=<',e,'>');
+  }
+}
+
+
+
+
+const onChangePosition = (elem) => {
+  try {
+    changeRangevalue(elem.value);
+    if(gElegWriter) {
+      //console.log('onClickChangePosition::elem.value=<',elem.value,'>');
+      const reqPosition = `pos:${elem.value}\n`;
+      const wBuff = new TextEncoder().encode(reqPosition);
+      //console.log('onClickChangePosition::wBuff=<',wBuff,'>');
+      gElegWriter.write(wBuff);
+    }
+  }
+  catch(e) {
+    console.log('onClickChangePosition::e=<',e,'>');
+  }
+}
+
+const changeRangevalue = (value) => {
+  const targetElem = document.getElementById('eleg-position-value-target-label');
+  targetElem.textContent = value;
+  updateRangevalue(value);
+}
+
+const updateRangevalue = (value) => {
+  //console.log('updateRangevalue::value=<',value,'>');
+  const labelElem = document.getElementById('eleg-position-value-label');
+  labelElem.textContent = value;
+ 
+ //console.log('updateRangevalue::value=<',value,'>');
+  const rangeElem = document.getElementById('eleg-position-value-range');
+  rangeElem.value = value;
+}
---
+++ motor_ctrl/sensored.bldc/sensored.bldc.ino
@@ -0,0 +1,226 @@
+#define DUMP_VAR(x)  { \
+  Serial.print(__LINE__);\
+  Serial.print("@@"#x"=<");\
+  Serial.print(x);\
+  Serial.print(">");\
+  Serial.print("\n");\
+}
+
+#define PORT_BRAKE 11
+#define PORT_CW 12
+#define PORT_PWM 3
+#define PORT_HALL 2
+
+
+
+#define CW() {\
+  iBoolMotorCWFlag = true;\
+  digitalWrite(PORT_CW,HIGH);\
+}
+
+#define CCW() {\
+  iBoolMotorCWFlag = false;\
+  digitalWrite(PORT_CW,LOW);\
+  }
+
+
+static long iHallTurnCounter = 0;
+
+void setup() {
+  // initialize port for motor control
+  pinMode(PORT_BRAKE, OUTPUT);
+  pinMode(PORT_CW, OUTPUT);
+  pinMode(PORT_PWM, OUTPUT);
+  
+  pinMode(PORT_HALL, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PORT_HALL),HallTurnCounterInterrupt , FALLING);
+
+  // clock prescale by 1, PWM frequency: 32KHz
+  TCCR2B = TCCR2B & 0xF8 | 0x01;
+ 
+  Serial.begin(115200);
+  DUMP_VAR("goto start!!!\n");
+  digitalWrite(PORT_BRAKE,LOW);
+  digitalWrite(PORT_CW,HIGH);
+  analogWrite(PORT_PWM, 0);
+}
+
+static int32_t iMainLoopCounter = 0;
+
+const static int32_t iMainLoopPrintA = 1024;
+const static int32_t iMainLoopPrintB = 16;
+const static int32_t iMainLoopPrintSkip = iMainLoopPrintA*iMainLoopPrintB;
+
+static long volatile iHallTurnRunStep = 0;
+static bool iBoolMotorCWFlag = false; //
+static const  int16_t iPositionByHallRangeLow = 50;
+static const  int16_t iPositionByHallRangeDistance = 152;
+static const  int16_t iPositionByHallRangeHigh = iPositionByHallRangeLow + iPositionByHallRangeDistance;
+static int16_t volatile iPositionByHall= -1;
+static uint8_t volatile iConstCurrentSpeed = 0;
+
+static bool iBoolRunCalibrate = false; //
+
+
+
+
+static int16_t volatile iTargetPositionByHall= -1;
+void caclTargetSpeed(void);
+
+void loop() {
+  caclTargetSpeed();
+ //printTurnCounter();
+  handleIncommingCommand();
+}
+
+void HallTurnCounterInterrupt(void) {
+  iHallTurnCounter++;
+  //DUMP_VAR(iHallTurnCounter);
+  if(iHallTurnRunStep > 0) {
+    iHallTurnRunStep--;
+  } else {
+    stopMotor();
+  }
+  if(iBoolMotorCWFlag) {
+    iPositionByHall++;
+  } else {
+    iPositionByHall--;
+  }
+  DUMP_VAR(iPositionByHall);
+  DUMP_VAR(iConstCurrentSpeed);
+  DUMP_VAR(iTargetPositionByHall);
+}
+
+
+
+
+
+
+void runLongCommand(char ch);
+
+void handleIncommingCommand(void) {
+  signed incomingByte = Serial.read();
+  if (incomingByte != -1) {
+    if(incomingByte == 'z') {
+      CW();
+      digitalWrite(PORT_CW,HIGH);
+    }
+    if(incomingByte == 'f') {
+      CCW();
+    }
+    if(incomingByte == 'b') {
+      digitalWrite(PORT_BRAKE,HIGH);
+    }
+    if(incomingByte == 'e') {
+      digitalWrite(PORT_BRAKE,LOW);
+    }
+    if(incomingByte == '0') {
+      analogWrite(PORT_PWM, 0);
+      digitalWrite(PORT_BRAKE,LOW);
+    }
+    if(incomingByte == 'g') {
+      startMotor();
+    }
+    runLongCommand(incomingByte);
+  }
+}
+
+static String gHandleStringCommand;
+void runLongCommand(char ch) {
+  gHandleStringCommand += ch;
+  if(ch == '\n') {
+    DUMP_VAR(gHandleStringCommand);
+    if(gHandleStringCommand.startsWith("calibrate")) {
+      runCalibrate();
+    }
+    if(gHandleStringCommand.startsWith("pos:")) {
+      auto posValue = gHandleStringCommand.substring(4, gHandleStringCommand.length());
+      posValue.trim();
+      DUMP_VAR(posValue);
+      auto pos = posValue.toInt();
+      runToPosion(pos);
+    }
+    gHandleStringCommand = "";
+  }
+}
+
+void runCalibrate(void) {
+  iBoolMotorCWFlag = false;
+  digitalWrite(PORT_CW,LOW);
+  iBoolRunCalibrate = true;
+  startMotor();
+}
+
+//const static uint8_t iConstMaxSpeed = 255;
+const static uint8_t iConstMaxSpeed = 64;
+const static uint8_t iConstMinSpeed = 12;
+const static uint8_t iConstBrakeDistance = 32;
+
+void runToPosion(int16_t position) {
+  if(position >= iPositionByHallRangeHigh ||position <= iPositionByHallRangeLow) {
+    return;
+  }
+  iTargetPositionByHall = position;
+  //DUMP_VAR(position);
+  int16_t currentPos = iPositionByHall;
+  //DUMP_VAR(currentPos);
+  int16_t diff = position - currentPos;
+  //DUMP_VAR(diff);
+  iHallTurnRunStep = abs(diff);
+  //DUMP_VAR(iHallTurnRunStep);
+  if(iHallTurnRunStep > 0) {
+    if(diff > 0) {
+      CW();
+    } else {
+      CCW();
+    }
+    uint32_t speed1 = iConstMaxSpeed;
+    uint32_t speed2 = iHallTurnRunStep;
+    uint32_t speed3 = iPositionByHallRangeDistance;
+    uint32_t speed32 = iConstMaxSpeed * iHallTurnRunStep/iConstBrakeDistance;
+    if(speed32 > iConstMaxSpeed) {
+      speed32 = iConstMaxSpeed;
+    }
+    if(speed32 < iConstMinSpeed) {
+      speed32 = iConstMinSpeed;
+    }
+    uint8_t speed8 = speed32 & 0xff;
+    /*
+    if(speed8 < iConstMinSpeed) {
+      speed8 = iConstMinSpeed;
+    }
+    */
+    iConstCurrentSpeed = speed8;
+    analogWrite(PORT_PWM, speed8);
+    digitalWrite(PORT_BRAKE,HIGH);
+  }
+}
+const static int8_t iConstTargetNear = 1;
+
+static int8_t iTargetNearCounter = 0;
+void caclTargetSpeed(void) {
+  if(iTargetPositionByHall == -1) {
+    return;
+  }
+  int8_t diff = abs(iTargetPositionByHall - iPositionByHall);
+  if(diff <= iConstTargetNear ) {
+    if(iTargetNearCounter++ > 1) {
+      iTargetPositionByHall = -1;
+    }
+    return;
+  }
+  iTargetNearCounter = 0;
+  runToPosion(iTargetPositionByHall);
+}
+
+void stopMotor(void) {
+  analogWrite(PORT_PWM, 0);
+  digitalWrite(PORT_BRAKE,LOW);
+  iHallTurnRunStep = -1;
+}
+void startMotor(void) {
+  analogWrite(PORT_PWM, 32);
+  iConstCurrentSpeed = 32;
+  digitalWrite(PORT_BRAKE,HIGH);
+  iHallTurnRunStep = 6;
+}
